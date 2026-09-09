b8eb5eba396e34808ebf1795804d7cfcd09c2817
quanghuy4112012-glitch <quanghuy4112012@gmail.com>
2026-09-09 12:45:15 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,4 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.meteordev.org/releases")
-        maven("https://maven.meteordev.org/snapshots")
-        mavenCentral()
-    }
-}
-
 rootProject.name = "lol-addon"
